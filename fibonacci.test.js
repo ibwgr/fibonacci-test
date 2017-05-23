f95ec9e9903368fd5030347296ea2b27b26bec23
Ueli Kunz <kunz@ideadapt.net>
2017-05-23 20:37:27 +0200

    add some more tests. fix array equals check (use deepEqual (or strictEqual to see effective failure))

--- fibonacci.test.js
+++ fibonacci.test.js
@@ -5,8 +5,19 @@ import fib from './fibonacci'
 
 describe('fibonacci', function(){
     describe('with n = 0', function(){
-        it('should return 0', function(){
-            assert.equal(0, fib(0))
+        it('should return an empty array', function(){
+            assert.deepEqual([], fib(0))
+        })
+    })
+
+    describe('with n = 5', function(){
+        it('should return the five correct fibonacci items', function(){
+            assert.deepEqual([0, 1, 1, 2, 3], fib(5))
+        })
+        describe('last item', function(){
+            it('should be 3', function(){
+                assert.strictEqual(3, fib(5).pop())
+            })
         })
     })
 })
