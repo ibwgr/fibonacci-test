6c3d46fa476fdd56771d635dac93c1e92f11c8de
Ueli Kunz <kunz@ideadapt.net>
2020-06-05 21:46:38 +0200

    add imports

--- fibonacci.test.mjs
+++ fibonacci.test.mjs
@@ -1,16 +1,17 @@
 import assert from 'assert'
 import {fibonacci} from './fibonacci.mjs'
+import {describe, it} from 'mocha'
 
 describe('fibonacci', function(){
     describe('with n = 0', function(){
         it('should return an empty array', function(){
-            assert.deepEqual([], fibonacci(0))
+            assert.deepStrictEqual([], fibonacci(0))
         })
     })
 
     describe('with n = 5', function(){
         it('should return the five correct fibonacci items', function(){
-            assert.deepEqual([0, 1, 1, 2, 3], fibonacci(5))
+            assert.deepStrictEqual([0, 1, 1, 2, 3], fibonacci(5))
         })
         describe('last item', function(){
             it('should be 3', function(){
